6aba2fb9fbe2cf2ec0268e22796ffe60566bb63a
Buckapi <112070278+buckapi@users.noreply.github.com>
2022-10-04 10:23:35 -0400

    correct

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -41,77 +41,7 @@ async function newTransaction(input){
     const newTransaction = input;
     const { 
         name, 
-        description, const Transaction= require ("../models/transaction"); 
-        const bcryptjs=require("bcryptjs");
-        const jwt=require("jsonwebtoken");
-        
-        async function getTransactionsByStatus(status,skip,limit){
-           const transactions = await Transaction.find()
-           .where({status})
-           .skip(skip)
-           .limit(limit)  
-           .sort({
-                name: 'asc'
-            })
-           ;
-           return transactions;
-        }
-        
-        async function getBestseller(bestseller,skip,limit){
-            const transactions = await Transaction.find()
-            .where({bestseller})
-            .skip(skip)
-            .limit(limit)  
-            .sort({
-                 name: 'asc'
-             })
-            ;
-            return transactions;
-         }
-         async function getDiscount(discount,skip,limit){
-            const transactions = await Transaction.find()
-            .where({discount})
-            .skip(skip)
-            .limit(limit)  
-            .sort({
-                 name: 'asc'
-             })
-            ;
-            return transactions;
-         }
-        
-        async function newTransaction(input){
-            const newTransaction = input;
-            const { 
-                name, 
-                description, 
-                price, 
-                ref,
-                presentation, 
-                category, 
-                currency, 
-                status,
-                discount,
-                bestseller,
-                images
-            }=newTransaction;
-        
-            try{
-                const transaction = new Transaction(newTransaction);
-                transaction.save();
-                return transaction; 
-            }
-            catch(error){
-                console.log(error);
-            }
-        }
-        
-        module.exports={
-            newTransaction,
-            getTransactionsByStatus,
-            getBestseller,
-            getDiscount,
-        };
+        description, 
         price, 
         ref,
         presentation, 
