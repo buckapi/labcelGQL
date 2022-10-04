9b8f56da75fdbf504d9dbbc4acd951c4394772e5
Buckapi <112070278+buckapi@users.noreply.github.com>
2022-10-04 01:53:35 -0400

    port

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function server () {
           ] 
 
     });   
-    serverApollo.listen().then(({url})=>{
+    serverApollo.listen({ port: 8000 }).then(({url})=>{
         console.log(`Server ON, ${url}`);            
     })  
 } 
